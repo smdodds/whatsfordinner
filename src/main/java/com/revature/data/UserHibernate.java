0097018251b88dad24ae1812a9d27b44fe73cb66
Keith Clark <K.C.Clark@eagle.clarion.edu>
2019-01-09 20:20:05 -0500

    implementation of UserHibernate finished

--- src/main/java/com/revature/data/UserHibernate.java
+++ src/main/java/com/revature/data/UserHibernate.java
@@ -1,8 +1,12 @@
 package com.revature.data;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-import org.hibernate.Hibernate;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import com.revature.beans.User;
 import com.revature.util.HibernateUtil;
@@ -12,37 +16,61 @@ public class UserHibernate implements UserDAO {
 
 	@Override
 	public User addUser(User u) {
-		// TODO Auto-generated method stub
-		return null;
+		Session s = hu.getSession();
+		Transaction tx = s.beginTransaction();
+		s.save(u);
+		tx.commit();
+		s.close();
+		return u;
 	}
 
 	@Override
 	public User getUserbyId(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Session s = hu.getSession();
+		User u = s.get(User.class, id);
+		s.close();
+		return u;
 	}
 
 	@Override
 	public User getUserByEmailAndPassword(User u) {
-		// TODO Auto-generated method stub
-		return null;
+		Session s = hu.getSession();
+		String query = "from com.revature.beans.User where EMAIL=:email and PASSWORD=:password";
+		Query<User> q = s.createQuery(query, User.class);
+		q.setParameter("email", u.getEmail());
+		q.setParameter("password", u.getPassword());
+		u = q.list().get(0);
+		s.close();
+		return u;
 	}
 
 	@Override
 	public Set<User> getUsers() {
-		// TODO Auto-generated method stub
-		return null;
+		Session s = hu.getSession();
+		String query = "from com.revature.beans.User";
+		Query<User> q = s.createQuery(query,User.class);
+		List<User> uList = q.getResultList();
+		s.close();
+		return new HashSet<User>(uList);
 	}
 
 	@Override
 	public User updateUser(User u) {
-		// TODO Auto-generated method stub
-		return null;
+		Session s = hu.getSession();
+		Transaction tx = s.beginTransaction();
+		s.update(u);
+		tx.commit();
+		s.close();
+		return u;
 	}
 
 	@Override
 	public void deleteUser(User u) {
-		// TODO Auto-generated method stub
+		Session s = hu.getSession();
+		Transaction tx = s.beginTransaction();
+		s.delete(u);
+		tx.commit();
+		s.close();
 		
 	}
 
