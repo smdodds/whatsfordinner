277e50ef0df4449e0262009d7d2eac2e416ab449
Keith Clark <K.C.Clark@eagle.clarion.edu>
2019-01-09 22:05:24 -0500

    user service layer and ng loginform

--- src/main/angular/src/app/app-routing.module.ts
+++ src/main/angular/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginformComponent  } from './loginform/loginform.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ {path: 'login', component: LoginformComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
