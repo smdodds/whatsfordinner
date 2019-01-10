7f4fcc02a48ef51e7845f9948e1a0a3cadd12d99
Keith Clark <K.C.Clark@eagle.clarion.edu>
2019-01-09 20:33:40 -0500

    user service interface and spring implementation

---
+++ src/main/java/com/revature/services/UserSpring.java
@@ -0,0 +1,46 @@
+package com.revature.services;
+
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.revature.beans.User;
+import com.revature.data.UserDAO;
+
+@Service
+public class UserSpring implements UserSevice {
+	@Autowired
+	private UserDAO ud;
+	@Override
+	public User addUser(User u) {
+		return ud.addUser(u);
+	}
+
+	@Override
+	public User getUserbyId(int id) {
+		return ud.getUserbyId(id);
+	}
+
+	@Override
+	public User login(User u) {
+		return ud.getUserByEmailAndPassword(u);
+	}
+
+	@Override
+	public Set<User> getUsers() {
+		return ud.getUsers();
+	}
+
+	@Override
+	public User updateUser(User u) {
+		return ud.updateUser(u);
+	}
+
+	@Override
+	public void deleteUser(User u) {
+		ud.deleteUser(u);
+		
+	}
+
+}
