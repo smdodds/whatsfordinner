7f2c513a57c8bff948fe38ad471787cc6ffc9a20
baeiten <baeiten@gmail.com>
2019-01-14 11:33:13 -0500

    continuing testing

--- src/test/java/com/revature/data/RecipeHibernateTest.java
+++ src/test/java/com/revature/data/RecipeHibernateTest.java
@@ -5,15 +5,24 @@ import static org.junit.Assert.*;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.transaction.Transactional;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.revature.beans.Recipe;
+import com.revature.utils.HibernateUtil;
 import com.revature.beans.Ingredient;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {RecipeHibernate.class, IngredientHibernate.class, HibernateUtil.class})
+@Transactional
 public class RecipeHibernateTest {
 	
 	private Recipe testRecipe = new Recipe();
-	private Recipe ingredient = new Recipe();
 
 	
 	Set<Ingredient> ingredients = new HashSet<Ingredient>();
@@ -22,15 +31,24 @@ public class RecipeHibernateTest {
 	private Ingredient testingredient2 = new Ingredient();
 	private Ingredient testingredient3= new Ingredient();
 
+	@Autowired
 	private IngredientDAO test1 = new IngredientHibernate();
+
+	@Autowired
 	private RecipeDAO test2 = new RecipeHibernate();
 	
 	{
-		System.out.println("Creating test example");		
-		
+		/*
+		testingredient1.setId(1);
+		testingredient2.setId(2);
+		testingredient3.setId(3);
 		testingredient1.setName("Peanut Butter");
 		testingredient2.setName("Jelly");
 		testingredient3.setName("Bread");
+		*/
+		testingredient1 = test1.getById(-7);
+		testingredient2 = test1.getById(-9);
+		testingredient3 = test1.getById(-8);
 		
 		ingredients.add(testingredient1);
 		ingredients.add(testingredient2);
@@ -43,8 +61,7 @@ public class RecipeHibernateTest {
 	
 	@Test
 	public void testAddRecipe(){
-		test1.save(testingredient1);
-		test1.save(testingredient2);
-		test1.save(testingredient3);
+		test2.saveRecipe(testRecipe);
+		test2.deleteRecipe(testRecipe);
 	}
 }
