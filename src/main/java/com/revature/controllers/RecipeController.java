e2b674470a5025ad659574c4d36d8c47a6409864
baeiten <baeiten@gmail.com>
2019-01-14 14:25:48 -0500

    added cross-origin

--- src/main/java/com/revature/controllers/RecipeController.java
+++ src/main/java/com/revature/controllers/RecipeController.java
@@ -6,7 +6,7 @@ import java.util.Set;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +18,7 @@ import com.revature.beans.User;
 import com.revature.data.RecipeDAO;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RequestMapping(value="/recipe")
 public class RecipeController {
 	@Autowired
