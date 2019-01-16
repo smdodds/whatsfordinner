74119f653286250eae091d4d7703aabff3c3452a
Gavin C. Greif <gavin.greif@gmail.com>
2019-01-16 15:03:29 -0500

    Implemented angular form validation

--- src/main/angular/src/app/createprofile/createprofile.component.ts
+++ src/main/angular/src/app/createprofile/createprofile.component.ts
@@ -1,32 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Profile } from './profile';
 
 @Component({
   selector: 'app-createprofile',
   templateUrl: './createprofile.component.html',
   styleUrls: ['./createprofile.component.css']
 })
-export class CreateprofileComponent implements OnInit {
-  firstname: string;
-  lastname: string;
-  username: string;
-  password: string;
-  email: string;
+export class CreateprofileComponent{
+
+  profile = new Profile(0,'','','','','', false);
 
   constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
-  createprofile():void{
-    console.log(this.firstname);
-    console.log(this.lastname);
-    console.log(this.username);
-    console.log(this.password);
-    console.log(this.email);
+  onSubmit():void{
+    console.log(this.profile.firstname);
+    console.log(this.profile.lastname);
+    console.log(this.profile.username);
+    console.log(this.profile.password);
+    console.log(this.profile.email);
+    console.log(this.profile.checkbox)
   }
 
-  cancel():void{
+  onCancel():void{
     console.log('cancel create profile clicked');
     this.router.navigate(['/login'])
   }
---
+++ src/main/angular/src/app/createprofile/profile.ts
@@ -0,0 +1,13 @@
+export class Profile {
+
+    constructor(
+      public id: number,
+      public firstname: string,
+      public lastname: string,
+      public username: string,
+      public password: string,
+      public email: string,
+      public checkbox: boolean
+    ) {  }
+  
+  }
